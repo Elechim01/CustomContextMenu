5de1c9013d4bb81712ac57eefc8966fb56ed40d1
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-21 22:46:13 +0200

    Initial Commit

---
+++ CustomContextMenu/CustomContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomContextMenuApp.swift
+//  CustomContextMenu
+//
+//  Created by Michele Manniello on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
